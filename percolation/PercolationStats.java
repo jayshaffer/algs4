e60c161e4e223f72253dcdbf163f6b724684bb94
Jay Shaffer <jayshaffer@Jays-MacBook-Pro.local>
2018-08-10 08:35:54 -0600

    percolation stuff

---
+++ percolation/PercolationStats.java
@@ -0,0 +1,27 @@
+class PercolationStats{
+    public PercolationStats(int n, int trials){
+        if(n <= 0 || trials <= 0){
+
+        }        
+    }
+
+    public double mean(){
+
+    }
+
+    public double stddex(){
+
+    }
+
+    public double confidenceLo(){
+
+    }
+
+    public double confidenceHi(){
+
+    }
+
+    public static void main(String[] args){
+
+    }
+}
